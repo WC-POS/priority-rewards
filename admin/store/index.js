102bef75daeb59af1a92cb498d6997c4deac9fd9
Devin Sharpe <devinrsharpe@gmail.com>
2021-10-03 22:20:34 -0400

    feat: add store with api, account, and franchise objects.

---
+++ admin/store/index.js
@@ -0,0 +1,49 @@
+import create from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+let apiStore = (set) => ({
+  api: {
+    slug: null,
+    token: {
+      key: null,
+      expiresAt: null,
+    },
+  },
+  setSlug: (slug) => {
+    set(() => ({ slug }));
+  },
+  setToken: (token) => {
+    set(() => ({ token }));
+  },
+  reset: () => {
+    set(() => ({
+      slug: null,
+      token: {
+        key: null,
+        expiresAt: null,
+      },
+    }));
+  },
+});
+
+let franchiseStore = (set) => ({
+  franchise: {},
+  setFranchise: (franchise) => {
+    set(() => ({ franchise: { ...franchise } }));
+  },
+});
+
+let accountStore = (set) => ({
+  account: {},
+  setAccount: (account) => {
+    set(() => ({ account: { ...account } }));
+  },
+});
+
+accountStore = devtools(accountStore);
+apiStore = devtools(apiStore);
+franchiseStore = devtools(franchiseStore);
+
+export const useAccountStore = create(accountStore);
+export const useAPIStore = create(apiStore);
+export const useFranchiseStore = create(franchiseStore);
