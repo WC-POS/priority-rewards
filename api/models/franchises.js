b2055438933620f636e50be44cbe897aec0613eb
Devin Sharpe <devinrsharpe@gmail.com>
2021-10-19 16:27:03 -0400

    feat: extend franchise model/

--- api/models/franchises.js
+++ api/models/franchises.js
@@ -8,6 +8,39 @@ module.exports = {
       superTitle: { type: String },
       subtitle: { type: String },
     },
+    logo: {
+      location: { type: String, nullable: true },
+      description: { type: String, nullable: true },
+      alternativeText: { type: String, nullable: true },
+    },
+    services: {
+      promotions: { type: Boolean, default: false },
+      events: { type: Boolean, default: false },
+      olo: { type: Boolean, default: false },
+    },
+    contact: {
+      website: { type: String, nullable: true },
+      email: { type: String, nullable: true },
+      phone: { type: String, nullable: true },
+    },
+    welcomeMessage: {
+      title: { type: String, nullable: true },
+      body: { type: String, nullable: true },
+    },
+    documents: {
+      EULA: { type: String, nullable: true },
+      termsOfUse: { type: String, nullable: true },
+    },
+    payment: {
+      provider: {
+        type: String,
+        choices: ["AUTHORIZE.NET", "STRIPE", null],
+        nullable: true,
+        default: null,
+      },
+      public: { type: String, nullable: true, default: null },
+      private: { type: String, nullable: true, default: null },
+    },
     slug: { type: String },
   },
 };
