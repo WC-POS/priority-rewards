5b7bb5071ef715df56b97d57c462b86701a670bf
Devin Sharpe <devinrsharpe@gmail.com>
2021-10-20 16:54:06 -0400

    feat [admin]: added get, post, put, and upload methods to API Store.

--- admin/store/index.js
+++ admin/store/index.js
@@ -1,13 +1,114 @@
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-let apiStore = (set) => ({
+let apiStore = (set, get) => ({
   api: {
     slug: null,
     token: {
       key: null,
       expiresAt: null,
     },
+  },
+  fetchGet: async (endpoint, options = {}) => {
+    const urlPoints = endpoint
+      .trim()
+      .split("/")
+      .filter((urlPoint) => urlPoint !== "");
+    let state = get();
+    let payload = {};
+    try {
+      const res = await fetch(
+        `https://${state.slug}.${
+          process.env.NEXT_PUBLIC_API_HOST
+        }/${urlPoints.join("/")}/`,
+        {
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${state.token.key}`,
+            ...options.headers,
+          },
+        }
+      );
+      try {
+        const data = await res.json();
+        payload = { status: res.status, ok: res.ok, body: data, error: null };
+      } catch (err) {
+        payload = { status: res.status, ok: res.ok, body: {}, error: err };
+      }
+    } catch (err) {
+      payload = { status: 999, ok: false, body: {}, error: err };
+    }
+    return payload;
+  },
+  fetchPost: async (endpoint, body, isPUT = false) => {
+    const urlPoints = endpoint
+      .trim()
+      .split("/")
+      .filter((urlPoint) => urlPoint !== "");
+    let state = get();
+    let payload = {};
+    try {
+      const res = await fetch(
+        `https://${state.slug}.${
+          process.env.NEXT_PUBLIC_API_HOST
+        }/${urlPoints.join("/")}/`,
+        {
+          method: isPUT ? "PUT" : "POST",
+          headers: {
+            authorization: `Bearer ${state.token.key}`,
+            "Content-Type": "application/json",
+          },
+          body: body ? JSON.stringify(body) : JSON.stringify({}),
+        }
+      );
+      try {
+        const data = await res.json();
+        payload = { status: res.status, ok: res.ok, body: data, error: null };
+      } catch (err) {
+        payload = { status: res.status, ok: res.ok, body: {}, error: err };
+      }
+    } catch (err) {
+      payload = { status: 999, ok: false, body: {}, error: err };
+    }
+    return payload;
+  },
+  fetchUpload: async (endpoint, data) => {
+    const urlPoints = endpoint
+      .trim()
+      .split("/")
+      .filter((urlPoint) => urlPoint !== "");
+    let state = get();
+    let payload = {};
+    try {
+      let formData = new FormData();
+      data.forEach((file) => {
+        console.log("file", file);
+        formData.append(file.name, file.data);
+      });
+      console.log(data);
+      console.log(formData);
+      const res = await fetch(
+        `https://${state.slug}.${
+          process.env.NEXT_PUBLIC_API_HOST
+        }/${urlPoints.join("/")}/`,
+        {
+          method: "POST",
+          headers: {
+            authorization: `Bearer ${state.token.key}`,
+          },
+          body: formData,
+        }
+      );
+      try {
+        const data = await res.json();
+        payload = { status: res.status, ok: res.ok, body: data, error: null };
+      } catch (err) {
+        payload = { status: res.status, ok: res.ok, body: {}, error: err };
+      }
+    } catch (err) {
+      payload = { status: 999, ok: false, body: {}, error: err };
+    }
+    return payload;
   },
   setSlug: (slug) => {
     set(() => ({ slug }));
