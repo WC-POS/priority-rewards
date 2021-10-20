34da7c0e613fda4ffffb729c87d720290f353fd5
Devin Sharpe <devinrsharpe@gmail.com>
2021-10-20 16:54:44 -0400

    feat [admin]: added filler to clubs page.

--- admin/pages/memberships/clubs.jsx
+++ admin/pages/memberships/clubs.jsx
@@ -1,5 +1,38 @@
+import { Heading, Stack, Text } from "@chakra-ui/react";
+
 const Clubs = () => {
-  return <div>Membership Clubs</div>;
+  return (
+    <Stack
+      w="full"
+      h="full"
+      direction="row"
+      alignItems="flex-start"
+      justifyContent="start"
+    >
+      <Stack
+        direction="column"
+        bgColor="white"
+        rounded="lg"
+        shadow="lg"
+        w={{ base: "50%", md: "25%" }}
+        p={4}
+      >
+        <Heading w="full" fontSize="2xl">
+          Clubs
+        </Heading>
+      </Stack>
+      <Stack
+        direction="column"
+        bgColor="white"
+        rounded="lg"
+        shadow="lg"
+        w={{ base: "50%", md: "75%" }}
+        p={4}
+      >
+        <Text w="full">Hello</Text>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Clubs;
