17639c1a15d4ae60092a909c6a56713bf970ef0b
Devin Sharpe <devinrsharpe@gmail.com>
2021-10-21 13:03:54 -0400

    feat [api]: added location routes to franchise routes file.

--- api/routes/admin/franchise.js
+++ api/routes/admin/franchise.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { Types } = require("mongoose");
 
 module.exports = async function (fastify, options) {
   fastify.get(
@@ -18,6 +19,7 @@ module.exports = async function (fastify, options) {
                 $ref: "models-franchises#/properties/documents",
               },
               logo: { $ref: "models-franchises#/properties/logo" },
+              services: { $ref: "models-franchises#/properties/services" },
             },
           },
         },
@@ -160,6 +162,180 @@ module.exports = async function (fastify, options) {
       }
       console.log(res);
       reply.code(200).send({ name: req.file.filename });
+    }
+  );
+  fastify.get(
+    "/location/:id/",
+    {
+      preValidation: [fastify.guards.isAdminAuthenticated],
+      schema: {
+        repsonse: {
+          200: {
+            $ref: "models-locations#",
+          },
+        },
+      },
+    },
+    async function (req, reply) {
+      try {
+        const location = await fastify.db.location.findById(
+          Types.ObjectId(req.params.id)
+        );
+        if (location) {
+          reply.code(200).send(location.toJSON());
+        } else {
+          reply.code(404).send({ error: "Location not found." });
+        }
+      } catch (err) {
+        reply.code(500).send({ error: err });
+      }
+    }
+  );
+  fastify.put(
+    "/location/:id/",
+    {
+      preValidation: [fastify.guards.isAdminAuthenticated],
+      schema: {
+        body: {
+          $ref: "models-locations#",
+        },
+        repsonse: {
+          200: {
+            $ref: "models-locations#",
+          },
+        },
+      },
+    },
+    async function (req, reply) {
+      try {
+        const location = await fastify.db.location.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: req.body }
+        );
+        reply.code(200).send(location.toJSON());
+      } catch (err) {
+        console.log(err);
+        reply.code(400).send({ error: err });
+      }
+    }
+  );
+  fastify.post(
+    "/location/:id/upload/previewImage/",
+    {
+      preHandler: fastify.upload.single("previewImage"),
+    },
+    async function (req, reply) {
+      let isValidLocation = false;
+      try {
+        isValidLocation = Boolean(
+          await fastify.db.location
+            .findById(Types.ObjectId(req.params.id))
+            .count()
+        );
+      } catch (err) {
+        console.log(err);
+      }
+      if (req.file && isValidLocation) {
+        try {
+          const key = `${req.scope.franchise.slug}/locations/${req.params.id}/previewImage/${req.file.filename}`;
+          await fastify.s3.putObject({
+            Bucket: process.env.AWS_BUCKET,
+            Key: key,
+            Body: fs.createReadStream(req.file.path),
+            ACL: "public-read",
+          });
+          const location = `https://${
+            process.env.AWS_BUCKET
+          }.${process.env.AWS_ENDPOINT.replace("https://", "")}/${key}`;
+          reply.code(200).send({ location });
+        } catch (err) {
+          console.log(err);
+          reply.code(500).send({ error: err });
+        }
+      } else {
+        reply
+          .code(400)
+          .send({ error: "Please include a file in your upload." });
+      }
+    }
+  );
+  fastify.get(
+    "/locations/",
+    {
+      preValidation: [fastify.guards.isAdminAuthenticated],
+      schema: {
+        body: {
+          type: ["object", "null"],
+          page: { type: "number" },
+          pageSize: { type: "number" },
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              count: { type: "number" },
+              page: { type: "number" },
+              maxPage: { type: "number" },
+              results: {
+                type: "array",
+                items: {
+                  type: "object",
+                  $ref: "models-locations#",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    async function (req, reply) {
+      const pageSize =
+        req.body && req.body.pageSize
+          ? req.body.pageSize
+          : process.env.PAGE_SIZE;
+      const page = req.body && req.body.page ? req.body.page : 1;
+      const list = await fastify.db.location
+        .find({ franchise: req.scope.franchise._id })
+        .sort({ name: 1 })
+        .skip(pageSize * (page - 1))
+        .limit(pageSize);
+      const queryCount = await fastify.db.location
+        .find({ franchise: req.scope.franchise._id })
+        .count();
+      reply.code(200).send({
+        count: queryCount,
+        page,
+        maxPage: Math.ceil(queryCount / pageSize),
+        results: list,
+      });
+    }
+  );
+  fastify.post(
+    "/locations/",
+    {
+      preValidation: [fastify.guards.isAdminAuthenticated],
+      schema: {
+        body: {
+          $ref: "models-locations#",
+        },
+        response: {
+          201: {
+            $ref: "models-locations#",
+          },
+        },
+      },
+    },
+    async function (req, reply) {
+      try {
+        let location = await fastify.db.location.create({
+          ...req.body,
+          franchise: req.scope.franchise._id,
+        });
+        reply.code(201).send(location.toJSON());
+      } catch (err) {
+        console.log(err);
+        reply.code(400).send({ error: err });
+      }
     }
   );
 };
