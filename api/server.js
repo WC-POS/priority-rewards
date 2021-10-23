26258c58755a98315284e70725c806efefa13d23
Devin Sharpe <devinrsharpe@gmail.com>
2021-10-23 17:02:17 -0400

    feat [api]: register paginate plugin.

--- api/server.js
+++ api/server.js
@@ -52,6 +52,7 @@ const start = async () => {
     app.register(require("./plugins/franchise-scope"));
     app.register(require("./plugins/s3-driver"));
     app.register(require("./plugins/admin-authentication"));
+    app.register(require("./plugins/paginate"));
     app.register(require("./plugins/route-loader"));
     app.listen(process.env.API_PORT, "0.0.0.0");
   } catch (err) {
