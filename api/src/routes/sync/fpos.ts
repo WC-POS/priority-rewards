0467bd46091e821f9b6c59d31b81d3401f3738f7
Devin Sharpe <devinrsharpe@gmail.com>
2021-12-08 09:57:13 -0500

    feat [api]: added temp fpos items get route.

--- api/src/routes/sync/fpos.ts
+++ api/src/routes/sync/fpos.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
-import { FPOSItemDocument } from "../../models/fpos-items";
 import { RouteWithAPIHeaders, RouteWithBody } from "../../utils";
+
+import { FPOSItemDocument } from "../../models/fpos-items";
 
 export default async function (
   fastify: FastifyInstance,
@@ -21,6 +22,26 @@ export default async function (
     function (req, reply) {
       console.log(req.headers["sync-private"]);
       reply.code(200).send(req.scope);
+    }
+  );
+
+  fastify.get<RouteWithAPIHeaders & RouteWithBody<FPOSItemDocument[]>>(
+    "/items/",
+    {
+      preValidation: [fastify.guards.isAPIAuthenticated],
+      schema: {
+        response: {
+          200: {
+            type: "array",
+            items: {
+              $ref: "Models-FPOSItem#",
+            },
+          },
+        },
+      },
+    },
+    async function (req, reply) {
+      reply.code(200).send(await fastify.db.models.FPOSItem.find());
     }
   );
 
